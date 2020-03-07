b316c1cedca868bbf9836afdf08d0e458306ace1
xiao <xiao@localhost>
2020-03-06 20:36:04 -0400

    intentionally failed module1_test.go as starting state

--- module1/module1_test.go
+++ module1/module1_test.go
@@ -1,8 +1,11 @@
 package module1
 
 import (
+	"bufio"
+	"log"
 	"os"
 	"path"
+	"strings"
 	"testing"
 )
 
@@ -14,4 +17,39 @@ func TestModule1CheckEnv(t *testing.T) {
 	if actual != expected0 && actual != expected1 {
 		t.Errorf("environment variable GOPATH not set properly")
 	}
+}
+
+func TestModule1CheckEnvGOOS(t *testing.T) {
+	found := OpenFileAndFindString("module1.txt", "GOOS=")
+
+	if !found {
+		t.Errorf("'go env' does not work as expected")
+	}
+}
+
+// OpenFileAndFindString opens a file and return if the given string is found or not
+func OpenFileAndFindString(filename string, expected string) bool {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		trimmed := strings.Trim(t, " ")
+		if trimmed == "" {
+			continue
+		}
+
+		// matching logic
+		if trimmed == expected {
+			return true
+		}
+	}
+
+	return false
 }
