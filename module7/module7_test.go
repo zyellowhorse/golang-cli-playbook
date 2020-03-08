e587f7b0bd743041321bb674f988654c3af0edff
xiao <xiao@localhost>
2020-03-08 18:37:37 -0300

    add unit tests to module7

---
+++ module7/module7_test.go
@@ -0,0 +1,60 @@
+package module7
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestModule7Import(t *testing.T) {
+	found := OpenFileAndFindString("module7_code.go", "import (")
+
+	if !found {
+		t.Errorf("import not found")
+	}
+}
+
+func TestModule7ImportFmt(t *testing.T) {
+	found := OpenFileAndFindString("module7_code.go", "	\"fmt\"")
+
+	if !found {
+		t.Errorf("package 'fmt' not imported")
+	}
+}
+
+func TestModule7ImportRuntime(t *testing.T) {
+	found := OpenFileAndFindString("module7_code.go", "	\"runtime\"")
+
+	if !found {
+		t.Errorf("package 'runtime' not imported")
+	}
+}
+
+// OpenFileAndFindString opens a file and return if the given string is found or not
+func OpenFileAndFindString(filename string, expected string) bool {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		trimmed := strings.Trim(t, " ")
+		if trimmed == "" {
+			continue
+		}
+
+		// matching logic
+		if trimmed == expected {
+			return true
+		}
+	}
+
+	return false
+}
