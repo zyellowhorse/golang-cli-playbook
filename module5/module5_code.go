589fee5297a39af7569609e0552a92dc648ac27d
xiao <xiao@localhost>
2020-03-08 19:06:41 -0300

    final state of module5_code.go

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,5 +1,10 @@
 package module5
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
