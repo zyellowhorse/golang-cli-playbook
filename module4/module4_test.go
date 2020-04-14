74d235d65ee857dce375efbce9d9732de92cd0e8
xiao <xiao@localhost>
2020-04-14 20:17:17 -0300

    update module4 with go-mod-copy workaround

--- module4/module4_test.go
+++ module4/module4_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestModule4GoFormatContent(t *testing.T) {
 	expected := "	github.com/codemodus/kace"
-	found := OpenFileAndFindNthString("../go.mod", 0, expected)
+	found := OpenFileAndFindNthString("./go.mod.copy", 0, expected)
 	if found != true {
 		t.Errorf("the kace package is not found")
 	}
