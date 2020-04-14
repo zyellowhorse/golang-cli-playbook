9bdca4f32222524a855167227c58236f2daf2fa1
xiao <xiao@localhost>
2020-04-14 20:08:11 -0300

    update module3 with go-mod-copy workaround

--- module3/module3_test.go
+++ module3/module3_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestModule3GoFormatContent(t *testing.T) {
 	expected := "	github.com/nathan-osman/go-sunrise"
-	found := OpenFileAndFindNthString("../go.mod", 0, expected)
+	found := OpenFileAndFindNthString("./go.mod.copy", 0, expected)
 	if found != true {
 		t.Errorf("the go-sunrise package is not found")
 	}
