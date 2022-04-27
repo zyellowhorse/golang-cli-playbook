6d5a1132116e8ae251d326e84c62472650831ffe
zyellowhorse <zacharyyellowhorse@gmail.com>
2022-04-26 18:24:15 -0600

    goimports

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,7 +1,11 @@
 package module5
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
-/*
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
@@ -10,4 +14,3 @@ func GetExampleDotCom() {
 
 	defer resp.Body.Close()
 }
-*/
